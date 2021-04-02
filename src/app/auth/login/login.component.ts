b3f0f776a2e03db971904ac803e693cf5f422bc2
Abhishek Jariwala <abhishekjari15@gmail.com>
2021-04-02 23:46:34 +0530

    saving data in store using auth reducer

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
-import {Store} from "@ngrx/store";
-
-import {AuthService} from "../auth.service";
-import {tap} from "rxjs/operators";
-import {noop} from "rxjs";
-import {Router} from "@angular/router";
+import {AuthService} from '../auth.service';
+import {Router} from '@angular/router';
+import {tap} from 'rxjs/operators';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../reducers';
+import {login} from '../auth.actions';
+import {noop} from 'rxjs';
 
 @Component({
   selector: 'login',
@@ -18,14 +19,16 @@ export class LoginComponent implements OnInit {
   form: FormGroup;
 
   constructor(
-      private fb:FormBuilder,
-      private auth: AuthService,
-      private router:Router) {
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router,
+    private store: Store<AppState>
+  ) {
 
-      this.form = fb.group({
-          email: ['test@angular-university.io', [Validators.required]],
-          password: ['test', [Validators.required]]
-      });
+    this.form = fb.group({
+      email: ['test@test.com', [Validators.required]],
+      password: ['test', [Validators.required]]
+    });
 
   }
 
@@ -34,7 +37,18 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    const formValue = this.form.value;
 
+    this.auth.login(formValue.email, formValue.password).pipe(
+      tap(user => {
+        console.log('user information : ', user);
+        this.store.dispatch(login({user}));
+        this.router.navigateByUrl('/courses');
+      })
+    ).subscribe(
+      noop,
+      error => alert(error)
+    );
   }
 
 }
